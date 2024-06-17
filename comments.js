c754fe283bda72bba1e3f745cab56e4c931f2fe6
Wei Wei <monkalways@gmail.com>
2024-06-17 23:44:33 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+// create web server
+const express = require('express');
+const app = express();
+
+// create a route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// listen to port 3000
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
